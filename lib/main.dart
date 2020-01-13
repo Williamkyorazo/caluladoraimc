aeecc127f16512e201dd417d6a09984fdf71bd35
William <willian.seu@gmail.com>
2020-01-13 20:20:55 -0300

    Envio dos Arquivos

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: CalculadoraIMC(),
+  ));
+}
+
+class CalculadoraIMC extends StatefulWidget {
+  @override
+  _CalculadoraIMCState createState() => _CalculadoraIMCState();
+}
+
+class _CalculadoraIMCState extends State<CalculadoraIMC> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Calculadora IMC"),
+        centerTitle: true,
+      ),
+      
+    );
+  }
+}
