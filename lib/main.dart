56206e6c0949d7921e347cec72ebad120fe8e8c2
William <willian.seu@gmail.com>
2020-01-13 21:01:26 -0300

    Ui pronta

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData.dark().copyWith(
+      inputDecorationTheme: InputDecorationTheme(enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.orange)
+      ))
+    ),
     home: CalculadoraIMC(),
   ));
 }
@@ -16,10 +22,62 @@ class _CalculadoraIMCState extends State<CalculadoraIMC> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Calculadora IMC"),
+        title: Text("Calculadora de IMC", style: TextStyle(color: Colors.orange),),
         centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {},
+          )
+        ],
       ),
-      
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(Icons.person_outline, size: 170.0, color: Colors.orange),
+            buildTextFiel("Peso (kg)"),
+            SizedBox(
+              height: 10.0,
+            ),
+            buildTextFiel("Altura (mt)"),
+            SizedBox(
+              height: 10.0,
+            ),
+            Container(
+              height: 50.0,
+              child: RaisedButton(
+                color: Colors.orange,
+                child: Text("Verificar", style: TextStyle(
+                  color: Colors.black, fontSize: 25.0
+                )),
+                onPressed: (){},
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text("Informe seus dados", style: TextStyle(
+              color: Colors.orange, fontSize: 20.0
+            ),
+            textAlign: TextAlign.center,)
+            
+          ],
+        ),
+      ),
     );
   }
+}
+
+Widget buildTextFiel(String label){
+  return TextField(
+      decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.orange, fontSize: 25.0),
+        border: OutlineInputBorder(),
+      ),
+      style: TextStyle(color: Colors.orange, fontSize: 25.0),
+      keyboardType: TextInputType.number,
+    );
 }
